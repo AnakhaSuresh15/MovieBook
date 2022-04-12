148305944d35fda3c56d0b0d0526b0a47ee973b6
Anakha Suresh <anakhasuresh15@gmail.com>
2022-04-12 18:37:02 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movies } from './Movies';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MovieBook';
+  mTitle?: string;
+  searchBox: string = "";
+  year?: string;
+  rated?: string;
+  released?: string;
+  runtime?: string;
+  display: boolean = false;
+  img?: string;
+  constructor(private readonly http: HttpClient) { }
+  searchFunc(){
+    this.getUrl().subscribe((data: Movies) => {
+      if(data.Response === "True"){
+        this.mTitle = data.Title;
+        this.img = data.Poster;
+        this.year = data.Year;
+        this.rated = data.Rated;
+        this.released = data.Released;
+        this.runtime = data.Runtime;
+        this.display = true;
+      }
+    });
+  }
+
+  getUrl(){
+    let url = 'http://www.omdbapi.com/?apikey=e5e07c78&t=' + this.searchBox;
+    return this.http.get<Movies>(url);
+  }
 }
+
